a8933f25ba3788a25ed0a3a5b912b59ac2699b3e
Hassaanw27 <hassaanw27@gmail.com>
2020-03-23 13:59:01 +0100

    added

--- java.js
+++ java.js
@@ -1,35 +1,33 @@
-$(window, document, undefined).ready(function() {
+function myFunction(x, _this) {
+  if (_this.checked) {
+    x.style.borderColor = 'rgba(255,0,0,1)';
+  } else  {
+    x.style.borderColor = 'rgba(255,0,0,0.2';
+  }
+}
 
-  $('input').blur(function() {
-    var $this = $(this);
-    if ($this.val())
-      $this.addClass('used');
-    else
-      $this.removeClass('used');
-  });
+function anotherFunction(x, _this) {
+  if (_this.checked) {
+    x.style.borderColor = 'rgba(0, 153, 51, 1)';
+  } else  {
+    x.style.borderColor = 'rgba(0, 153, 51, 0.2)';
+  }
+}
 
-  var $ripples = $('.ripples');
+function athirdFunction(x, _this) {
+  if (_this.checked) {
+    x.style.borderColor = 'rgba(0, 0, 255, 1)';
+    document.getElementById("checkbox").checked = false;
+  } else  {
+    x.style.borderColor = 'rgba(0, 0, 255, 0.2)';
+  }
 
-  $ripples.on('click.Ripples', function(e) {
+}
 
-    var $this = $(this);
-    var $offset = $this.parent().offset();
-    var $circle = $this.find('.ripplesCircle');
-
-    var x = e.pageX - $offset.left;
-    var y = e.pageY - $offset.top;
-
-    $circle.css({
-      top: y + 'px',
-      left: x + 'px'
-    });
-
-    $this.addClass('is-active');
-
-  });
-
-  $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
-  	$(this).removeClass('is-active');
-  });
-
-});
+function cbChange(obj) {
+    var cbs = document.getElementsByClassName("cb");
+    for (var i = 0; i < cbs.length; i++) {
+        cbs[i].checked = false;
+    }
+    obj.checked = true;
+}
