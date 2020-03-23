18264a2c9dc86f49b774174ae0eef3d132a15c2d
Hassaanw27 <hassaanw27@gmail.com>
2020-03-23 19:27:59 +0100

    suck ass

--- java.js
+++ java.js
@@ -1,33 +1,53 @@
 function myFunction(x, _this) {
+  uncheckall(_this)
   if (_this.checked) {
     x.style.borderColor = 'rgba(255,0,0,1)';
-  } else  {
-    x.style.borderColor = 'rgba(255,0,0,0.2';
+  } else {
+    x.style.borderColor = 'rgba(255,0,0,0.2)';
   }
 }
 
 function anotherFunction(x, _this) {
+  uncheckall(_this)
   if (_this.checked) {
     x.style.borderColor = 'rgba(0, 153, 51, 1)';
-  } else  {
+  } else {
     x.style.borderColor = 'rgba(0, 153, 51, 0.2)';
   }
 }
 
 function athirdFunction(x, _this) {
+  uncheckall(_this)
   if (_this.checked) {
     x.style.borderColor = 'rgba(0, 0, 255, 1)';
-    document.getElementById("checkbox").checked = false;
-  } else  {
+    //document.getElementById('winterpreset').checked = false
+    //document.getElementById('nytkort').style.borderColor = 'rgba(255,0,0,0.2)';
+  } else {
     x.style.borderColor = 'rgba(0, 0, 255, 0.2)';
   }
-
+}
+function afourthFunction(x, _this) {
+  uncheckall(_this)
+  if (_this.checked) {
+    x.style.borderColor = 'rgba(220, 220, 0, 1)';
+  } else {
+    x.style.borderColor = 'rgba(200, 200, 0, 0.2)';
+  }
 }
 
-function cbChange(obj) {
-    var cbs = document.getElementsByClassName("cb");
-    for (var i = 0; i < cbs.length; i++) {
-        cbs[i].checked = false;
-    }
-    obj.checked = true;
+function uncheckall(_this) {
+  var liste = [
+    document.getElementById('nytkort'),
+    document.getElementById('nytkort2'),
+    document.getElementById('nytkort3'),
+    document.getElementById('nytkort4')
+  ]
+  for (var i = 0; i < liste.length; i++) {
+    liste[i].getElementsByTagName('input')[0].checked = false;
+  }
+  liste[0].style.borderColor = 'rgba(0, 153, 51, 0.2)';
+  liste[1].style.borderColor = 'rgba(255, 0, 0, 0.2)';
+  liste[2].style.borderColor = 'rgba(0, 0, 255, 0.2)';
+  liste[3].style.borderColor = 'rgba(200, 200, 0, 0.2)';
+  _this.checked = true
 }
